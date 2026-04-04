e697e235acd17889041a5ee25d7b62a74b76dd59
onedot9191 <hyong4115@naver.com>
2026-04-04 11:04:07 +0900

    Update provider.ts

--- src/lib/dictionary/provider.ts
+++ src/lib/dictionary/provider.ts
@@ -1,7 +1,6 @@
-import type { DictionaryEntry, DictionarySuggestion, SyllableResult } from './models';
+import type { DictionaryEntry, SyllableResult } from './models';
 
 export interface DictionaryProvider {
   lookupEntry(word: string): Promise<DictionaryEntry>;
   lookupSyllables(word: string): Promise<SyllableResult>;
-  suggestEntries(word: string): Promise<DictionarySuggestion[]>;
 }
